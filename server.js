28a238bdd9c911d2dbe601a054b2bca788a33f24
Isaias <impomales@gmail.com>
2016-08-13 05:37:22 +0000

    renders a homepage with app description

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 
+app.use(express.static(path.join(__dirname + '/html')))
+
 app.get('/', function(req, res) {
-    res.send('hello world!')
+    res.render('index.html')
 })
 
 app.listen(8080, function() {
