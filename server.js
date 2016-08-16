38a74bf68dd0e62a4bdee7d4a77bce1c0fb86291
Isaias <impomales@gmail.com>
2016-08-16 20:11:17 +0000

    set up for deployment

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/:date', function(req, res) {
     }
 })
 
-app.listen(8080, function() {
+app.listen(process.env.PORT || 8080, function() {
     console.log('the app is listening at port 8080!')
 })
